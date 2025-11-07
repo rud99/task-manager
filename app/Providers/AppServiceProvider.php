38b6c4e7b11f5ec3a6cf6ef21dc7107d2b87e59f
Mikhail Zakharov <mz-83@mail.ru>
2025-11-07 12:36:14 +0400

    eager loading, страховка от деструктивных команд, логирование длинных запросов

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonInterval;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Http\Kernel;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,30 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // предотвратить выполнение команд, таких как migrate:fresh!
+        DB::prohibitDestructiveCommands(app()->isProduction());
+
+        Model::shouldBeStrict(! app()->isProduction());
+        // строчка выше, заменяет 3 нижние
+        //        Model::preventLazyLoading(! app()->isProduction());
+        //        Model::preventSilentlyDiscardingAttributes(! app()->isProduction());
+        //        Model::preventAccessingMissingAttributes(! app()->isProduction());
+
+        if (app()->isProduction()) {
+            // долгий запрос БД
+            DB::listen(function ($query) {
+                if ($query->time > 200) {
+                    logger()->debug('DB Query longer than 2s: '.$query->sql, $query->bindings);
+                }
+            });
+
+            // долгий запрос HTTP
+            app(Kernel::class)->whenRequestLifecycleIsLongerThan(
+                CarbonInterval::seconds(4),
+                function () {
+                    logger()->debug('Request longer than 4s: '.request()->url());
+                }
+            );
+        }
     }
 }
